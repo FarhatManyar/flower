13f74982755b55c5f5fd4ef0b036594d3ddc9db2
FarhatManyar <farhat.b.manyar@gmail.com>
2026-06-12 19:52:25 +0530

    Fix: remove duplicate default export in Navbar.jsx

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+export default function Navbar() {
+  return (
+   
+   
+   <div>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <a href="/ourservice">Our Service</a>
+   </div>
+  )
+}
